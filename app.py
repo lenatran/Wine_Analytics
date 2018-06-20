ce2edf372a891a84f9240b316764d3e11113927d
Janaki <jdkora@gmail.com>
2018-06-19 17:41:46 -0700

    bubble chart and top10 data updates made

--- app.py
+++ app.py
@@ -58,87 +58,51 @@ def counts():
     return jsonify(con_cleanedList)
 
 @app.route("/varieties/<variety>")
-@app.route("/varieties/<variety>/<country>")
-def varcon(variety=None, country=None):
+def varcon(variety):
     
-
     # Select statement
     sel = [Wine.title, Wine.price, Wine.country, Wine.winery]
 
-    if not country:
-        # Get top 10 wine variety based on variety
-        results = session.query(*sel).\
-            filter(Wine.variety == variety).order_by(desc(Wine.points)).limit(10)
-        
-        wine_data = []
-        for result in results:
-            cur_dic = {}
-            cur_dic["Title"] = result[0]
-            cur_dic["Price"] = result[1]
-            cur_dic["Country"] = result[2]
-            cur_dic["Winery"] = result[3]
-            wine_data.append(cur_dic)
-            
-        return jsonify(wine_data)
-
     # Get top 10 wine variety based on variety and country
     results = session.query(*sel).\
-        filter(Wine.variety == variety).\
-        filter(Wine.country == country).order_by(desc(Wine.points)).limit(10)
+        filter(Wine.variety == variety).order_by(desc(Wine.points)).limit(10)
     
     wine_data = []
     for result in results:
         cur_dic = {}
         cur_dic["Title"] = result[0]
         cur_dic["Price"] = result[1]
+        cur_dic["Country"]=result[2]
         cur_dic["Winery"] = result[3]
         wine_data.append(cur_dic)
             
     return jsonify(wine_data)
     
 @app.route('/wine/<variety>')
-@app.route('/wine/<variety>/<country>')
-def winevarcon(variety=None, country=None):
+def winevar(variety):
   
-    if not country:
     # Select statement
-        sel = [Wine.country, Wine.points, Wine.price, Wine.title]
+    sel = [Wine.country, Wine.points, Wine.price, Wine.title]
 
-        # Get data based on variety
-        results = session.query(*sel).\
-            filter(Wine.variety == variety).all()
-
-    
-        country = [result[0] for result in results]
-        points = [result[1] for result in results]
-        price = [result[2] for result in results]
-        title = [result[3] for result in results]
-    
-        variety_data = [{
-            "country": country,
-            "points": points,
-            "price": price,
-            "title": title
-        }]
-    return jsonify(variety_data)
-        
-    # Get data based on variety and country
-    sel = [Wine.points, Wine.price, Wine.title]
-  
+    # Get data based on variety
     results = session.query(*sel).\
-        filter(Wine.variety == variety).\
-        filter(Wine.country == country).all()
-    points = [result[0] for result in results]
-    price = [result[1] for result in results]
-    title = [result[2] for result in results]
-    
-    data = [{
+        filter(Wine.variety == variety).all()
+
+
+    country = [result[0] for result in results]
+    points = [result[1] for result in results]
+    price = [result[2] for result in results]
+    title = [result[3] for result in results]
+
+    variety_data = [{
+        "country": country,
         "points": points,
         "price": price,
-       "title": title
-       }]
-    return jsonify(data)
-
+        "title": title
+    }]
+    return jsonify(variety_data)
+        
+   
 # route for pie chart data (country count)
 @app.route("/country_count/<variety>")
 def country_count(variety):
@@ -159,10 +123,22 @@ def country_count(variety):
     }]
     return jsonify(data)
 
+
+@app.route("/coords")
+def coordinate():
+    stmt = session.query(Coordinate).statement
+    region_df = pd.read_sql_query(stmt, session.bind)
+    region_df=region_df[region_df["coordinates"].notnull()]
+    data = [{
+       "region": region_df["region_1"].values.tolist(),
+       "country": region_df["country"].values.tolist(),
+       "coordinates": region_df["coordinates"].values.tolist()
+   }]
+    return jsonify(data)
+
 @app.route("/mapWine/<variety>")
 @app.route("/mapWine/<variety>/<country>")
 def mapwinevarcon(variety=None, country=None):
-    
 
     # Select statement
     sel = [Wine.country, Wine.points, Wine.price, Wine.title, Wine.region_1, Wine.winery]
@@ -183,7 +159,7 @@ def mapwinevarcon(variety=None, country=None):
         "winery": wine_df["winery"].values.tolist()
         }]
         return jsonify(data)
-        
+
 
     # Get data based on variety and country
     results = session.query(*sel).\
@@ -191,7 +167,7 @@ def mapwinevarcon(variety=None, country=None):
         filter(Wine.country == country).all()
     wine_df = pd.DataFrame(results, columns=["country", "points","price","title", "region", "winery"])
     wine_df = wine_df[wine_df["region"].notnull()]
-   # wine_df["price"].fillna(0, inplace=True);
+    # wine_df["price"].fillna(0, inplace=True);
     data = [{
         "country": wine_df["country"].values.tolist(),
         "points": wine_df["points"].values.tolist(),
@@ -202,18 +178,6 @@ def mapwinevarcon(variety=None, country=None):
         }]
     return jsonify(data)
 
-@app.route("/coords")
-def coordinate():
-    stmt = session.query(Coordinate).statement
-    region_df = pd.read_sql_query(stmt, session.bind)
-    region_df=region_df[region_df["coordinates"].notnull()]
-    data = [{
-       "region": region_df["region_1"].values.tolist(),
-       "country": region_df["country"].values.tolist(),
-       "coordinates": region_df["coordinates"].values.tolist()
-   }]
-    return jsonify(data)
-    
 if __name__ == "__main__":
     app.run(debug=True)
         
